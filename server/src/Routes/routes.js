c4302831627676f4baf4ee016f2e0dcda1d6d3b9
thiago morgado <thiago_morgado@hotmail.com>
2024-10-26 01:19:51 -0300

    Refactor: Code Reafactor controllers

--- server/src/Routes/routes.js
+++ server/src/Routes/routes.js
@@ -1,24 +1,21 @@
 import { Router } from "express";
-import controllerDoctor from "../controllers/controller.doctor.js";
-import controllerUser from "../controllers/controle.user.js"
-import jwt from '../Utils/token.js'
+import { controllerDoctors } from "../controllers/controller.doctor.js";
+import controllerUser from "../controllers/controle.user.js";
+import jwt from "../Utils/token.js";
 
 const router = Router();
 
-
-router.post("/users/register", controllerUser.Inserir)
-router.post("/users/login", controllerUser.Login)
-
+router.post("/users/register", controllerUser.Inserir);
+router.post("/users/login", controllerUser.Login);
 
 // Rotas a baixo so Funciona se eu Informar o Token
 router.use(jwt.ValidateToken);
 
-router.get("/doctors", controllerDoctor.Listar);
-router.post("/doctors", controllerDoctor.Inserir);
-router.put("/doctors/:id_doctor", controllerDoctor.Editar);
-router.delete("/doctors/:id_doctor", controllerDoctor.Excluir);
+router.get("/doctors", controllerDoctors.Listar);
+router.post("/doctors", controllerDoctors.Inserir);
+router.put("/doctors/:id_doctor", controllerDoctors.Editar);
+router.delete("/doctors/:id_doctor", controllerDoctors.Excluir);
 
-
-
+router.get("/doctors/:id_doctor/services", controllerDoctors.ListarServicos);
 
 export default router;
--- server/src/controllers/controller.doctor.js
+++ server/src/controllers/controller.doctor.js
@@ -1,41 +1,45 @@
 import serviceDoctor from "../services/service.doctor.js";
 
 
-async function Listar(req, res) {
-  const name = req.query.name
+export const controllerDoctors = {
+  async Listar(req, res) {
+    const name = req.query.name
 
-  const doctors = await serviceDoctor.Listar(name);
+    const doctors = await serviceDoctor.Listar(name);
 
-  res.status(200).json(doctors);
+    res.status(200).json(doctors);
+  },
+
+  async Inserir(req, res) {
+    const { name, specialty, icon } = req.body;
+    const createDoctor = await serviceDoctor.Inserir(name, specialty, icon)
+
+
+    return res.status(201).json(createDoctor);
+  },
+  async Editar(req, res) {
+    const { id_doctor } = req.params;
+    const { name, specialty, icon } = req.body;
+
+    const doctor = await serviceDoctor.Editar(id_doctor, name, specialty, icon)
+
+
+    return res.status(200).json(doctor)
+  },
+  async Excluir(req, res) {
+    const { id_doctor } = req.params;
+
+    const doctor = await serviceDoctor.Excluir(id_doctor)
+
+
+    return res.status(201).json(doctor)
+
+  },
+
+  async ListarServicos(req, res) {
+    const { id_doctor } = req.params
+  }
+
 }
 
-async function Inserir(req, res) {
-  const { name, specialty, icon } = req.body;
-  const createDoctor = await serviceDoctor.Inserir(name, specialty, icon)
 
-
-  return res.status(201).json(createDoctor);
-}
-async function Editar(req, res) {
-  const { id_doctor } = req.params;
-  const { name, specialty, icon } = req.body;
-
-  const doctor = await serviceDoctor.Editar(id_doctor, name, specialty, icon)
-
-
-  return res.status(200).json(doctor)
-}
-async function Excluir(req, res) {
-  const { id_doctor } = req.params;
-
-  const doctor = await serviceDoctor.Excluir(id_doctor)
-
-
-  return res.status(201).json(doctor)
-
-}
-
-
-
-
-export default { Listar, Inserir, Editar, Excluir };
